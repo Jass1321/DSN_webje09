60c53bb3fd869cbe17ef95ca90bdd76f7c5f0667
alvarofabian0432 <alvaro.motta@tecsup.edu.pe>
2021-05-13 20:11:07 -0500

    primer y último commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const Handlebars = require('handlebars');
+
+//Initializarions
+const app = express();
+require('./database');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),    
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(require('./routes'));
+module.exports = app;
